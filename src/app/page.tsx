573bfa23c69035fb75916f9ae4a071986b81aa8f
Xspark <151998806+xsprk@users.noreply.github.com>
2025-08-22 16:53:53 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
 import { InstagramVideoForm } from "@/features/instagram/components/form";
+"use client";
+import { useEffect } from "react";
 
+export default function Page() {
+  useEffect(() => {
+    fetch("/api/log-user");
+  }, []);
+
+  return <h1>Hello World</h1>;
+}
 export default function HomePage() {
   return (
     <div className="flex flex-col py-8">

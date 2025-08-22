bd4401f9a704318e5ade0dda99427fca6544a937
Xspark <151998806+xsprk@users.noreply.github.com>
2025-08-22 12:26:26 +0530

    Add files via upload

---
+++ src/lib/constants.ts
@@ -0,0 +1,19 @@
+type NavLink = {
+  name: string;
+  href: string;
+};
+
+export const siteLinks: NavLink[] = [
+  {
+    name: "Home",
+    href: "/",
+  },
+  {
+    name: "Terms of Service",
+    href: "/terms-of-service",
+  },
+  {
+    name: "Privacy Policy",
+    href: "/privacy-policy",
+  },
+];

08e545ae9ff4e94c2080ce7454dda1d978b301e7
Xspark <151998806+xsprk@users.noreply.github.com>
2025-08-22 11:57:17 +0530

    Add files via upload

---
+++ src/app/layout.tsx
@@ -0,0 +1,57 @@
+import { Metadata } from "next";
+import { DM_Sans as FontSans } from "next/font/google";
+
+import { Navbar, Footer } from "@/components/layout";
+import { AdSense } from "@/components/adsense/AdSense";
+import { ThemeProvider } from "@/components/providers/theme-provider";
+import { ReactQueryProvider } from "@/components/providers/react-query-provider";
+
+import { cn } from "@/lib/utils";
+
+import "./globals.css";
+
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
+
+export const metadata: Metadata = {
+  title: "Instagram Video Downloader",
+  description: "Download Instagram Videos",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head>
+        <AdSense pId="4868101519453026"/>
+      </head>
+      <body
+        className={cn(
+          fontSans.variable,
+          "overflow-x-hidden bg-background font-sans antialiased"
+        )}
+      >
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <ReactQueryProvider>
+            <Navbar />
+            <main className="relative h-[calc(100vh-6rem)] overflow-y-auto px-2 sm:px-4">
+              {children}
+            </main>
+            <Footer />
+          </ReactQueryProvider>
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}

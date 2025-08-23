bef42e112419c2ecd5d507b022efcdda165da868
Xspark <151998806+xsprk@users.noreply.github.com>
2025-08-23 10:14:26 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function HomePage() {
   return (
     <div className="flex flex-col py-8">
       <h1 className="text-balance mb-8 text-center text-4xl font-extrabold">
-        Instagram Reels Downloader
+        Instagram Downloader
       </h1>
       <section className="flex flex-col items-center justify-center gap-4">
         <InstagramVideoForm />
